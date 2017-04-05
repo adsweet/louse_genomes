996cc7fc565e0f1bbdd85b5907d8df8e59eb0511
Drew Sweet <dulcisad@gmail.com>
2017-04-05 13:53:05 -0500

    Update changefastqnames.py

--- changefastqnames.py
+++ changefastqnames.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env Python3
 
 #################################################################################################
-#This script changes the FASTQ header names of the output files from the mapping pipeline 
-#(reference_mapping_pipeline_script.sh).
+# This script changes the FASTQ header names of the output files from the mapping pipeline 
+# (reference_mapping_pipeline_script.sh).
 
-#Usage: python3 changefastqnames.py infile outfile sample_name 
+# Usage: python3 changefastqnames.py infile outfile sample_name 
 
-#Written by Andrew D. Sweet
+# Written by Andrew D. Sweet
 #################################################################################################
 
 import sys
@@ -17,7 +17,7 @@ out = open(sys.argv[2], 'w')
 name = sys.argv[3]
 
 for line in input:
-	x = re.sub(r'^\@(PHUM\d+\.Pheme)',r'@' + name + r'.\1',line) #Change the regex according to the headers
+	x = re.sub(r'^\@(PHUM\d+\.Pheme)',r'@' + name + r'.\1',line) # Change the regex according to the headers
 	out.write(x)
 
 
