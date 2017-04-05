976ddceda99b5cafe067fb2cf05452a37837e624
Drew Sweet <dulcisad@gmail.com>
2017-04-05 13:54:40 -0500

    Update fq2fa.py

--- fq2fa.py
+++ fq2fa.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env Python3
 
 #################################################################################################
-#This script converts FASTQ files to FASTA files.
+# This script converts FASTQ files to FASTA files.
 
-#Usage: python3 fq2fa.py infile outfile 
+# Usage: python3 fq2fa.py infile outfile 
 
-#Written by Andrew D. Sweet
+# Written by Andrew D. Sweet
 #################################################################################################
 
 
 from Bio import SeqIO
 import sys
+import os
 
-input = open(sys.argv[1])
-output = open(sys.argv[2],'w')
+os.system("cat *.fq >all_fastq.fq")
+
+input = open('all_fastq.fq')
+output = open(sys.argv[1],'w')
 
 SeqIO.convert(input, 'fastq', output, 'fasta')
