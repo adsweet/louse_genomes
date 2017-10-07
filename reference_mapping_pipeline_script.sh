ba85574e786a24f0b6de4666ca3b95e617df978c
Drew Sweet <dulcisad@gmail.com>
2017-10-06 21:04:21 -0500

    Update reference_mapping_pipeline_script.sh

--- reference_mapping_pipeline_script.sh
+++ reference_mapping_pipeline_script.sh
@@ -16,7 +16,7 @@ mypath='PATH TO LIBRARY';
 
 #RUN BOWTIE2
 bowtie2-build $myrefpath/$myref.fa $myrefpath/$myref.Genes; # THIS ONLY NEEDS TO BE RUN ONCE PER REFERENCE SET; COMMENT OUT OTHERWISE
-bowtie2 --phred33 --local -x $myrefpath/$myref.Genes -U $mypath/READS1,$mypath/READS2 -S $mylib.sam; 
+bowtie2 --phred33 --local -x $myrefpath/$myref.Genes -U $mypath/<READS1>,$mypath/<READS2> -S $mylib.sam; # CHANGE READS1 AND READS2 TO THE PAIRED-END READ FILES; WITHOUT TRIMMING USE -1 AND -2
 samtools faidx $myrefpath/$myref.fa; # THIS ONLY NEEDS TO BE RUN ONCE PER REFERENCE SET; COMMENT OUT OTHERWISE
 samtools view -bt $myrefpath/$myref.fa.fai $mylib.sam > $mylib.bam;
 samtools view -b -F 4 $mylib.bam > $mylib.mapped.bam;
@@ -25,7 +25,7 @@ samtools sort $mylib.mapped.bam $mylib.mapped.sorted;
 #CREATING VCF FILES AND CONSENSUS SEQUENCE
 samtools mpileup -uf $myrefpath/$myref.fa $mylib.mapped.sorted.bam -d 75 -Q 28 -q 10 > $mylib.mpileup
 bcftools view -cg $mylib.mpileup > $mylib.samtools.vcf;
-perl PATH/vcfutils.pl vcf2fq $mylib.samtools.vcf > $mylib.samtools.consensus.fq; # THIS IS FOR GETTING A CONSENSUS BEFORE VCF FILTERING
+perl <PATH>/vcfutils.pl vcf2fq $mylib.samtools.vcf > $mylib.samtools.consensus.fq; # THIS IS FOR GETTING A CONSENSUS BEFORE VCF FILTERING; CHANGE PATH TO THE VCFUTILS.PL SCRIPT
 samtools depth $mylib.mapped.sorted.bam | awk '{sum+=$3; sumsq+=$3*$3} END { print "Average =",sum/NR; print "Stdev =",sqrt(sumsq/NR - (sum/NR)**2)}' >> $mylib.depth.txt; # CALCULATE AVERAGE AND STDEV DEPTH
 
 #CREATE A PICARD .DICT FILE FOR THE REFERENCE SEQUENCE
