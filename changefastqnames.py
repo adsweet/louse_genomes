aca64bc99b63f30333918a862477addeb31ecb64
Drew Sweet <dulcisad@gmail.com>
2017-10-25 15:44:46 -0500

    Update changefastqnames.py

--- changefastqnames.py
+++ changefastqnames.py
@@ -4,21 +4,21 @@
 # This script changes the FASTQ header names of the output files from the mapping pipeline 
 # (reference_mapping_pipeline_script.sh).
 
-# Usage: python3 changefastqnames.py infile outfile sample_name 
+# Usage: python3 changefastqnames.py
 
 # Written by Andrew D. Sweet
 #################################################################################################
 
-import sys
 import re
+import glob
 
-input = open(sys.argv[1])
-out = open(sys.argv[2], 'w')
-name = sys.argv[3]
-
-for line in input:
-	x = re.sub(r'^\@(PHUM\d+\.Pheme)',r'@' + name + r'.\1',line) # Change the regex according to the headers
-	out.write(x)
+for file in glob.glob('*.samtools.consensus.fq'): # Change this depending on file extension
+	name = re.sub(r'(\S+).samtools.consensus.fq',r'\1',file) # Change this depending on file extension
+	with open(name + '.renamed.samtools.consensus.fq', 'w') as out:
+		with open(file, 'r') as input:
+			for line in input:
+				x = re.sub(r'^\@(PHUM\d+\.Pheme)',r'@' + name + r'.\1',line) # Change the regex according to the headers
+				out.write(x)
 
 
 
